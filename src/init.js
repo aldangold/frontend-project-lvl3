b25f5c58a702f1d96922157de1f4dce2ecf43b15
aldangold <meloshnikoff@gmail.com>
2022-01-06 16:06:36 +0300

    step2: added validation

--- src/init.js
+++ src/init.js
@@ -1,32 +1,99 @@
-import 'bootstrap';
+/* eslint-disable no-param-reassign, no-console  */
 import * as yup from 'yup';
+import onChange from 'on-change';
+
+const handleProcessErrors = (elements, processErrors) => {
+  switch (processErrors) {
+    case 'novalid':
+      elements.feedback.innerHTML = 'Ссылка должна быть валидным URL';
+      break;
+
+    case 'notOneOf':
+      elements.feedback.innerHTML = 'Уже загружен';
+      break;
+
+    default:
+      // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
+      throw new Error(`Unknown process state: ${processErrors}`);
+  }
+};
+
+const handleProcessState = (elements, processState) => {
+  switch (processState) {
+    case 'sent':
+      elements.inputField.classList.remove('is-invalid');
+      elements.feedback.classList.remove('text-danger');
+      elements.inputField.value = '';
+      elements.inputField.focus();
+      elements.feedback.classList.add('text-success');
+      elements.feedback.innerHTML = 'URL успешно загружен';
+      break;
+
+    case 'error':
+      elements.feedback.classList.remove('text-success');
+      elements.feedback.classList.add('text-danger');
+      elements.inputField.classList.add('is-invalid');
+      elements.inputField.focus();
+      break;
+
+    default:
+      // https://ru.hexlet.io/blog/posts/sovershennyy-kod-defolty-v-svitchah
+      throw new Error(`Unknown process state: ${processState}`);
+  }
+};
+
+const render = (elements) => (path, value) => {
+  switch (path) {
+    case 'form.processState':
+      handleProcessState(elements, value);
+      break;
+
+    case 'form.error':
+      handleProcessErrors(elements, value);
+      break;
+
+    default:
+      break;
+  }
+};
 
 export default () => {
-  const formElement = document.querySelector('.rss-form');
-  const feedbackElement = document.querySelector('.feedback');
-  const areaInput = formElement.querySelector('.form-control');
-  const handle = (e) => {
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    inputField: document.getElementById('url-input'),
+    submitButton: document.querySelector('button[type="submit"]'),
+    feedback: document.querySelector('.feedback'),
+    containerFeeds: document.querySelector('.container-xxl').querySelector('.feeds'),
+  };
+
+  const state = onChange({
+    form: {
+      processState: 'waiting',
+      valid: '',
+      error: '',
+    },
+    feeds: {
+      processState: '',
+      urls: [],
+    },
+  }, render(elements));
+
+  const validate = (url, list) => {
+    const schema = yup.string().url('novalid').notOneOf(list, 'notOneOf');
+    try {
+      schema.validateSync(url);
+      state.form.processState = 'sent';
+      state.feeds.urls.push(url);
+    } catch (err) {
+      state.form.processState = 'error';
+      state.form.error = err.message;
+    }
+  };
+
+  elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    validate(url, feedbackElement, areaInput);
-  };
-  formElement.addEventListener('submit', handle);
+    validate(url, state.feeds.urls);
+  });
 };
-
-const schema = yup.string().url();
-
-  const validate = (url, element, input) => {
-    try {
-      schema.validateSync(url);
-      input.classList.remove('is-invalid');
-      element.classList.remove('text-danger');
-      element.classList.add('text-success');
-      element.innerHTML = 'URL успешно загружен';
-    } catch (err) {
-      element.classList.remove('text-success');
-      element.classList.add('text-danger');
-      input.classList.add('is-invalid');
-      element.innerHTML = 'Ссылка должна быть валидным URL'
-    }
-  };
