295ce6c4343d544ed4e5f9aea0d959cc00356f51
aldangold <meloshnikoff@gmail.com>
2022-01-12 00:24:04 +0300

    step4: added render feeds and posts

--- src/init.js
+++ src/init.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-param-reassign, no-console  */
 import * as yup from 'yup';
-import onChange from 'on-change';
 import i18next from 'i18next';
-import render from './view';
+import onChange from 'on-change';
+import axios from 'axios';
+import render, { renderRSS } from './view';
 
 const resources = {
   ru: {
@@ -14,6 +14,8 @@ const resources = {
         networkError: 'Ошибка сети',
       },
       success: 'URL успешно загружен',
+      feeds: 'Фиды',
+      posts: 'Потоки',
     },
   },
 };
@@ -21,6 +23,38 @@ const resources = {
 const getValidator = () => {
   const schema = yup.string().url('noValid');
   return (url, feeds) => schema.notOneOf(feeds, 'notOneOf').validateSync(url);
+};
+
+const getProxyUrl = (url) => {
+  const proxyUrl = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
+  proxyUrl.searchParams.set('disableCache', 'true');
+  proxyUrl.searchParams.set('url', url);
+  return proxyUrl.toString();
+};
+
+const parseContent = (rssContent) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(rssContent, 'application/xml');
+
+  const error = doc.querySelector('parserError');
+  if (error) {
+    throw new Error(error.textContent);
+  }
+
+  const feedTitle = doc.querySelector('title').textContent;
+  const feedDescription = doc.querySelector('description').textContent;
+
+  const postElements = Array.from(doc.querySelectorAll('item'));
+  const items = postElements.map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+    return {
+      title, description, link,
+    };
+  });
+
+  return { feedTitle, feedDescription, items };
 };
 
 export default () => i18next.init({
@@ -43,9 +77,9 @@ export default () => i18next.init({
     const elements = {
       form: document.querySelector('.rss-form'),
       inputField: document.getElementById('url-input'),
-      submitButton: document.querySelector('button[type="submit"]'),
       feedback: document.querySelector('.feedback'),
-      containerFeeds: document.querySelector('.container-xxl').querySelector('.feeds'),
+      containerFeeds: document.querySelector('.feeds'),
+      containerPosts: document.querySelector('.posts'),
     };
 
     const watchedState = onChange(state, render(elements));
@@ -58,8 +92,22 @@ export default () => i18next.init({
 
       try {
         validate(url, state.feeds.urls);
-        watchedState.form.processState = 'sent';
-        watchedState.feeds.urls.push(url);
+
+        watchedState.feeds.processState = 'init';
+        axios.get(getProxyUrl(url))
+          .then((response) => {
+            renderRSS(elements, parseContent(response.data.contents));
+            watchedState.form.processState = 'sent';
+            watchedState.feeds.urls.push(url);
+          })
+          .catch((error) => {
+            if (error.isAxiosError) {
+              watchedState.form.error = 'networkError';
+            } else {
+              watchedState.form.error = 'dataError';
+            }
+            watchedState.form.processState = 'error';
+          });
       } catch (err) {
         watchedState.form.processState = 'error';
         watchedState.form.error = err.message;
--- src/view.js
+++ src/view.js
@@ -1,25 +1,92 @@
 import i18next from 'i18next';
 
-const handleErrors = (elements, processErrors) => {
+const renderError = (elements, processErrors) => {
+  elements.feedback.classList.add('text-danger');
+  elements.inputField.classList.add('is-invalid');
+  elements.inputField.focus();
   elements.feedback.textContent = i18next.t(`errors.${processErrors}`);
+};
+
+const renderSuccess = (elements) => {
+  elements.inputField.classList.remove('is-invalid');
+  elements.feedback.classList.remove('text-danger');
+  elements.inputField.value = '';
+  elements.inputField.focus();
+  elements.feedback.classList.add('text-success');
+  elements.feedback.textContent = i18next.t('success');
+};
+
+const initContainer = (elementName, nameContainer) => {
+  const divOuter = document.createElement('div');
+  const divInner = document.createElement('div');
+  const ul = document.createElement('ul');
+  const h2 = document.createElement('h2');
+
+  h2.textContent = i18next.t(nameContainer);
+
+  h2.classList.add('card-title', 'h4');
+  divInner.classList.add('card-body');
+  divOuter.classList.add('card', 'border-0');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+
+  divInner.append(h2);
+  divOuter.append(divInner);
+  divOuter.append(ul);
+  elementName.append(divOuter);
+};
+
+const addFeed = (elements, title, description) => {
+  const ul = elements.containerFeeds.querySelector('.list-group');
+  const li = document.createElement('li');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+
+  h3.textContent = title;
+  p.textContent = description;
+  li.classList.add('list-group-item', 'border-0', 'border-end-0');
+  h3.classList.add('h6', 'm-0');
+  p.classList.add('m-0', 'small', 'text-black-50');
+
+  h3.append(p);
+  li.append(h3);
+  ul.prepend(li);
+};
+
+const addPost = (elements, items) => {
+  const ul = elements.containerPosts.querySelector('.list-group');
+  items.forEach((item) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = item.title;
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    a.classList.add('fw-bold');
+    a.setAttribute('href', item.link);
+    a.setAttribute('target', '_blank');
+
+    li.append(a);
+    ul.prepend(li);
+  });
+};
+
+export const renderRSS = (elements, data) => {
+  const { feedTitle, feedDescription, items } = data;
+  addFeed(elements, feedTitle, feedDescription);
+  addPost(elements, items);
 };
 
 const handleProcessState = (elements, processState) => {
   switch (processState) {
     case 'sent':
-      elements.inputField.classList.remove('is-invalid');
-      elements.feedback.classList.remove('text-danger');
-      elements.inputField.value = '';
-      elements.inputField.focus();
-      elements.feedback.classList.add('text-success');
-      elements.feedback.textContent = 'URL успешно загружен';
+      renderSuccess(elements);
       break;
 
     case 'error':
       elements.feedback.classList.remove('text-success');
-      elements.feedback.classList.add('text-danger');
-      elements.inputField.classList.add('is-invalid');
-      elements.inputField.focus();
+      break;
+
+    case 'init':
+      initContainer(elements.containerFeeds, 'feeds');
+      initContainer(elements.containerPosts, 'posts');
       break;
 
     default:
@@ -35,7 +102,11 @@ const render = (elements) => (path, value) => {
       break;
 
     case 'form.error':
-      handleErrors(elements, value);
+      renderError(elements, value);
+      break;
+
+    case 'feeds.processState':
+      handleProcessState(elements, value);
       break;
 
     default:
